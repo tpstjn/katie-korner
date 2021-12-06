bb55936f5bc1c15ad02463ad1c735a718c6ade3c
barnesge19 <barnesge19@gcc.edu>
2021-12-06 13:19:07 -0500

    moving things

--- static/scripts/load_register_employee.js
+++
@@ -1,8 +0,0 @@
-// window.addEventListener("DOMContentLoaded", function() {
-//     const registerButton = document.getElementById("btnSubmit");
-//     registerButton.addEventListener("click", submitForm);
-// });
-
-// function submitForm() {
-//     window.location.href = "/register-employee/";
-// }
